f3606b7b28f6b72b7527b64893157b9a4352432e
Xmsyd <masayidalfaizqi@gmail.com>
2023-02-28 10:09:59 +0700

    menambahkan tugas dan paktikum

---
+++ Pemrograman-Web-2/Pertemuan 1/coba1.php
@@ -0,0 +1,11 @@
+<?php
+// definisikan konstanta
+    define('PHI', 3.14);
+    define('DBNAME', 'Inventory');
+    define('DBSERVER', 'localhost');
+
+    echo 'PHI nya : '.PHI;
+    echo '<br>Nama database nya : '.DBNAME;
+    echo '<br>Lokasi database nya : '.DBSERVER;
+
+?>
