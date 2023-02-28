8b32c7a6bfd7ba9a25c04241bce078ead4ed4fbc
Xmsyd <masayidalfaizqi@gmail.com>
2023-02-28 10:13:11 +0700

    menambahkan tugas php dasar

---
+++ Pemrograman-Web-2/Pertemuan 1/index.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = 'Muhamad Masayid Alfaizqi';
+    $umur = 18;
+    $berat = 45;
+
+    echo 'Nama saya : ' .$nama;
+    echo '<br>Umur saya : ' .$umur;
+    echo '<br>Berat saya : ' .$berat;
