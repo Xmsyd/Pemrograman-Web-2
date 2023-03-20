213cc30ab433fff5f5ff67a8d7ebf88221edfdf4
Xmsyd <masayidalfaizqi@gmail.com>
2023-03-20 19:17:57 +0700

    belajar php include dan recuired one

---
+++ Pemrograman-Web-2/Pertemuan-4/addSiswa.php
@@ -0,0 +1,52 @@
+<?php 
+    include_once 'template/header.php';
+?>
+
+
+<form method="POST" action="home.php">
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Nama Siswa</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <input id="nama" name="nama" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text1" class="col-4 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="uas" name="uas" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text2" class="col-4 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="uts" name="uts" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select" class="col-4 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="matkul" name="matkul" class="custom-select">
+        <option value="rabbit">Pemrograman Web</option>
+        <option value="duck">Basis Data</option>
+        <option value="fish">UIUX</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text3" class="col-4 col-form-label">Rombel</label> 
+    <div class="col-8">
+      <input id="rombel" name="rombel" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+<?php 
+    include_once 'template/footer.php';
+?>
---
+++ Pemrograman-Web-2/Pertemuan-4/home.php
@@ -0,0 +1,31 @@
+<?php 
+    include_once 'template/header.php';
+
+?>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Nilai UAS</th>
+      <th scope="col">Nilai UTS</th>
+      <th scope="col">Mata Kuliah</th>
+      <th scope="col">Rombel</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>1</th>
+      <th>Samsudin</th>
+      <th>100</th>
+      <th>99</th>
+      <th>UIUX</th>
+      <th>TI05</th>
+    </tr>
+  </tbody>
+</table>
+
+<?php 
+    include_once 'template/footer.php';
+?>
