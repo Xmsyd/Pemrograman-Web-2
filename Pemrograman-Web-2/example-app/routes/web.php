d23dab1bf6a364cd690a5aa8eed587eb6e39163e
Xmsyd <masayidalfarizqi@gmail.com>
2023-06-20 13:17:15 +0700

    Praktikum 13

--- Pemrograman-Web-2/example-app/routes/web.php
+++ Pemrograman-Web-2/example-app/routes/web.php
@@ -74,41 +74,53 @@ Route::prefix('toko') -> group(function(){
     Route::get('/profile',
         [TokoController::class, 'profile']);
 
-    // P11
+    // P13
+    Route::group(['middleware' => ['auth']], function(){
+
+         // P11
     Route::get('/admin',
     [TokoController::class, 'admin'])->name('produk.admin');
+
+     // tugas P11
+     Route::get('/customer',
+     [TokoController::class, 'customer'])->name('customer.customer');
+
+ // Praktikum 12
+ Route::prefix('produk')->group(function () {
+ Route::get('create', 
+     [TokoController::class, 'create'])->name('produk.create');
+
+ Route::post('/', 
+     [TokoController::class, 'store'])->name('produk.store');
+
+ Route::get('/{product}/edit',
+ [TokoController::class, 'edit'])->name('produk.edit');
+
+ Route::delete('/{product}',
+ [TokoController::class, 'destroy'])->name('produk.destroy');
+
+ Route::put('/{product}',
+ [TokoController::class, 'update'])->name('produk.update');
+ });
+
+ // Tugas P12
+ // membuat grup agar mencegah bentrok pada fungsi/url/route yang dipanggil
+ // route disini memiliki fungsinya masing masing seperti
+ // route get untuk mengambil data, post untuk insert, delete untuk menghapus, dan put untuk mengupdate
+ Route::prefix('customer')->group(function () {
+ Route::get('create',            [TokoController::class, 'create2'  ])  ->name  ('customer.create');
+ Route::post('/',                [TokoController::class, 'store2'   ])  ->name  ('customer.store');
+ Route::get('/{customer}/edit',  [TokoController::class, 'edit2'    ])  ->name  ('customer.edit');
+ Route::delete('/{customer}',    [TokoController::class, 'destroy2' ])  ->name  ('customer.destroy');
+ Route::put('/{customer}',       [TokoController::class, 'update2'  ])  ->name  ('customer.update');
+ });
+
+});
+
+   
     
-    // tugas P11
-    Route::get('/customer',
-        [TokoController::class, 'customer'])->name('customer.customer');
-
-    // Praktikum 12
-    Route::prefix('produk')->group(function () {
-    Route::get('create', 
-        [TokoController::class, 'create'])->name('produk.create');
-
-    Route::post('/', 
-        [TokoController::class, 'store'])->name('produk.store');
-
-    Route::get('/{product}/edit',
-    [TokoController::class, 'edit'])->name('produk.edit');
-
-    Route::delete('/{product}',
-    [TokoController::class, 'destroy'])->name('produk.destroy');
-
-    Route::put('/{product}',
-    [TokoController::class, 'update'])->name('produk.update');
-    });
-
-    // Tugas P12
-    // membuat grup agar mencegah bentrok pada fungsi/url/route yang dipanggil
-    // route disini memiliki fungsinya masing masing seperti
-    // route get untuk mengambil data, post untuk insert, delete untuk menghapus, dan put untuk mengupdate
-    Route::prefix('customer')->group(function () {
-    Route::get('create',            [TokoController::class, 'create2'  ])  ->name  ('customer.create');
-    Route::post('/',                [TokoController::class, 'store2'   ])  ->name  ('customer.store');
-    Route::get('/{customer}/edit',  [TokoController::class, 'edit2'    ])  ->name  ('customer.edit');
-    Route::delete('/{customer}',    [TokoController::class, 'destroy2' ])  ->name  ('customer.destroy');
-    Route::put('/{customer}',       [TokoController::class, 'update2'  ])  ->name  ('customer.update');
-    });
+   
 });          
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
